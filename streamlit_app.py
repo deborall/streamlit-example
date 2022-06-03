abb6fde8e2c9dfa3b06eff216b565faa6c2140f8
deborall <dave.eborall@gmail.com>
2022-06-03 10:05:56 +0200

    s3 Connect

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import s3fs
 
 """
 # Welcome to Streamlit!
@@ -14,37 +15,47 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+fs = s3fs.S3FileSystem(anon=False)
+
+def read_file(filename):
+    with fs.open(filename) as f:
+        return f.read().decode("utf-8")
+
 def get_UN_data():
-    AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-    df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-    return df.set_index("Region")
+    
+    #AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
+    content = read_file("sanne-eod/CashAccount_20220413_SLTWWF.csv")
+    df = pd.read_csv(content)
+    return df.set_index("InstrumentCode")
 
 try:
     df = get_UN_data()
     countries = st.multiselect(
-        "Choose countries", list(df.index), ["China", "United States of America"]
+        "Choose countries", list(df.index), ["ZAR", "SLFA1"]
     )
     if not countries:
-        st.error("Please select at least one country.")
+        st.error("Please select at least one account.")
     else:
         data = df.loc[countries]
-        data /= 1000000.0
-        st.write("### Gross Agricultural Production ($B)", data.sort_index())
+        st.table(data)
+        #data /= 1000000.0
+        #st.write("### Gross Agricultural Production ($B)", data.sort_index())
 
-        data = data.T.reset_index()
-        data = pd.melt(data, id_vars=["index"]).rename(
-            columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-        )
-        chart = (
-            alt.Chart(data)
-            .mark_area(opacity=0.3)
-            .encode(
-                x="year:T",
-                y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                color="Region:N",
-            )
-        )
-        st.altair_chart(chart, use_container_width=True)
+        # data = data.T.reset_index()
+        # data = pd.melt(data, id_vars=["index"]).rename(
+        #     columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
+        # )
+        # chart = (
+        #     alt.Chart(data)
+        #     .mark_area(opacity=0.3)
+        #     .encode(
+        #         x="year:T",
+        #         y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
+        #         color="Region:N",
+        #     )
+        # )
+        # st.altair_chart(chart, use_container_width=True)
+
 except URLError as e:
     st.error(
         """
